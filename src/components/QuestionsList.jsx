01efe2ab8c20102d338ea32d86741557d18e4443
Marco William Silva <marcowilliamdf@gmail.com>
2019-11-09 18:41:53 -0600

    Sort questions in alphabetical order

--- src/components/QuestionsList.jsx
+++ src/components/QuestionsList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
@@ -7,15 +7,40 @@ import Collapse from '@material-ui/core/Collapse';
 export default function QuestionsList({ questions }) {
     const classes = useStyles();
     const [showAnswer, setShowAnswer] = useState({});
+    const [isSortingAsc, setIsSortingAsc] = useState(undefined);
+    const [questionsList, setQuestionsList] = useState(questions);
+    const sortedQuestions = useMemo(() => [...questions].sort(({ question }, { question: previewQuestion }) => {
+        return question < previewQuestion ? -1 : 1;
+    }), [questions]);
+
+    useEffect(() => {
+        if (isSortingAsc == undefined) {
+            setQuestionsList(questions);
+        } else if (isSortingAsc) {
+            setQuestionsList(sortedQuestions);
+        } else {
+            setQuestionsList([...sortedQuestions].reverse());
+        }
+    }, [questions])
 
     const handleQuestionClick = (id) => () => {
         setShowAnswer((answers) => ({ ...answers, [id]: !answers[id] }));
     };
 
+    const sortQuestions = () => {
+        if (isSortingAsc == undefined) {
+            setQuestionsList(sortedQuestions);
+            setIsSortingAsc(true);
+        } else {
+            setQuestionsList([...questionsList].reverse());
+            setIsSortingAsc(!isSortingAsc);
+        }
+    }
+
     return (
         <>
             <Typography variant='h5'> Created Questions </Typography>
-            {questions.map(({ id, question, answer }) => (
+            {questionsList.map(({ id, question, answer }) => (
                 <div key={id} className={classes.questionsRow} onClick={handleQuestionClick(id)}>
                     <Typography className={classes.questionText}> {question} </Typography>
                     <Collapse in={showAnswer[id]}>
@@ -24,7 +49,7 @@ export default function QuestionsList({ questions }) {
                 </div>
             ))}
 
-            <Button variant="contained" color="primary" className={classes.button}>
+            <Button variant="contained" color="primary" className={classes.button} onClick={sortQuestions}>
                 Sort Questions
             </Button>
             <Button variant="contained" color="secondary" className={classes.button}>
